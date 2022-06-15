687210ad55c772fde1ad426a8a7dd61111aa8339
João Pster <joaopsterdev@gmail.com>
2022-06-15 19:49:18 -0300

    Resolvido o Problema do GitHub Pages | #VQV :rocket:

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <HashRouter basename="/">
+      <App />
+    </HashRouter>
   </BrowserRouter>,
   document.getElementById('root'),
 );
